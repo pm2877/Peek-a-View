ad39679f01d36f00e13adcb9c10709007017c94b
Parth Mehta <parth94@hotmail.com>
2019-01-31 22:45:07 -0800

    script running on all pages

--- background.js
+++ background.js
@@ -1,11 +1,22 @@
 /* globals chrome, console */
 'use strict';
 
+var peekingEnabled = false;
 chrome.browserAction.onClicked.addListener(updateIcon);
 chrome.browserAction.onClicked.addListener(function(tab) {
     enablePeeking(tab.id);    
 });
-  
+
+// chrome.tabs.onActivated.addListener(function(activeInfo) {
+//     console.log(activeInfo.tabId);
+//     enablePeeking(activeInfo.id); 
+// });
+
+// chrome.tabs.onUpdated.addListener(function(activeInfo) {
+//     console.log(activeInfo.tabId);
+//     enablePeeking(activeInfo.id); 
+// });
+
 function updateIcon() {
     chrome.browserAction.setIcon({path: 'images/inactive_16.png'});
 };
@@ -33,6 +44,19 @@ function enablePeeking(tabId) {
 
 // chrome.runtime.onInstalled.addListener();
 // chrome.runtime.onStartup.addListener();
+// window.open("popup.html", "extension_popup", "width=300, height=400, status=no, scrollbars=yes, resizable=yes");
+
+// chrome.windows.create({
+//     url: chrome.runtime.getURL("popup.html"),
+//     type: "popup",
+//     left: 200,
+//     top: 200,
+//     width: 400,
+//     height: 550
+// });
+
+// $("body").append('Test');
+
 chrome.notifications.onClicked.addListener(function(notificationId) {
     chrome.notifications.clear(notificationId);
 });
